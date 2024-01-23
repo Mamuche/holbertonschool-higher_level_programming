b872569b28301a0be79e0aed2e31bbbb05576db4
Mamuche <marionlaroche17@gmail.com>
2024-01-23 14:27:11 +0100

    string format

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print(chr(i), end='')
+for i in range(ord('a'), ord('z')+1):
+   print("{:c}".format(i), end="")
+# autre façon de faire :
+#
+# for i in range(97, 123):
+#    print(chr(i), end='')
