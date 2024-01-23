a03ed6cbe5a987f948b53c3244ac7b770724b137
Mamuche <marionlaroche17@gmail.com>
2024-01-23 14:34:14 +0100

    other possibilities

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i == 101 or i == 113:
+for letter in range(97, 123):
+    if letter == 101 or letter == 113:
         continue
-    print(chr(i), end='')
+    print("{:c}".format(letter), end='')
+# autre façon de faire :
+# for i in range(97, 123):
+#    if i == 101 or i == 113:
+#        continue
+#    print(chr(i), end='')
