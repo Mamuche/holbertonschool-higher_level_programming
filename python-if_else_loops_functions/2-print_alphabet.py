461dbc6f216b307a37dd48c472acb8b75680f706
Mamuche <marionlaroche17@gmail.com>
2024-01-23 14:29:09 +0100

    fix string format

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-   print("{:c}".format(i), end="")
-# autre façon de faire :
-#
-# for i in range(97, 123):
-#    print(chr(i), end='')
+for letter in range(97, 123):
+    print("{:c}".format(letter), end='')
