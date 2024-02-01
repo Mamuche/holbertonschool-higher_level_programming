90b3df670ca6ff672e2d3e2601cdc08e87b306ca
Mamuche <marionlaroche17@gmail.com>
2024-02-01 15:09:00 +0100

    requierement

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    cpy0 = my_list
     cpy1 = my_list.copy()
     for i in cpy1:
         if idx == cpy1.index(i):
@@ -8,6 +7,6 @@ def new_in_list(my_list, idx, element):
             cpy1.insert(idx, element)
             return(cpy1)
         elif idx < 0:
-            return(cpy1)
-        elif idx >= len(cpy1):
-            return(cpy1)
+            return(my_list)
+        elif idx >= len(my_list):
+            return(my_list)
