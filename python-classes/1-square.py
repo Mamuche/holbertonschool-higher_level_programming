5744c5353f222ecd8dc9fe6a48c8a0d61653ef63
Mamuche <marionlaroche17@gmail.com>
2024-02-05 10:15:05 +0100

    task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""Write a class Square with private instance atribute """
+
+
+class Square:
+
+    """class square"""
+
+    def __init__(self, size):
+
+        """instantiation"""
+        self.__size = 12
