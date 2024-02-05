d80074a7acf0019bdcfc8472d48d8d6c630ddd0b
Mamuche <marionlaroche17@gmail.com>
2024-02-05 10:51:45 +0100

    task3

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""Write a class Square with private instance atribute """
+
+
+class Square:
+    """class square"""
+    def __init__(self, size=0):
+        """instantiation"""
+        self.__size = size
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """return the area of square"""
+        return self.__size * self.__size
