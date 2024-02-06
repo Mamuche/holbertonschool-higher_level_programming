5ca7954b058562aea575f7f1e0b05db66aaecd8a
Mamuche <marionlaroche17@gmail.com>
2024-02-06 13:57:48 +0100

    task3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
+
+"""function that prints My name is <first name> <last name>"""
+
+
 def say_my_name(first_name, last_name=""):
+    """print depending first and last name"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
