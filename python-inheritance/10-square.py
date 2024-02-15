06e0b2221c210a5fbe9b0c4cfaf1a41062756db1
Mamuche <marionlaroche17@gmail.com>
2024-02-15 14:24:50 +0100

    task10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""define class rectangle from class BasseGeometry"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """new class inherits from Rectangle"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+    
+    def area(self):
+        """return the area of square"""
+        return(self.__size * self.__size)
+    
