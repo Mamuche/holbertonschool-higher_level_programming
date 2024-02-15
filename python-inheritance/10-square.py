b8f81549f1d517bfd5654a1f1330a04745c12d2c
Mamuche <marionlaroche17@gmail.com>
2024-02-15 15:33:21 +0100

    add super()

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,8 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """new class inherits from Rectangle"""
     def __init__(self, size):
-        super().__init__(size, size)
-        self.integer_validator("size", size)
+        super().integer_validator("size", size)
         self.__size = size
 
     def area(self):
