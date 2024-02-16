054e25a642ff5affd6b58f1c4ea8ac8d7d6e8aec
Mamuche <marionlaroche17@gmail.com>
2024-02-16 17:19:40 +0100

    requierements test

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle(BaseGeometry):
 
     def area(self):
         """return the area of rectangle"""
-        return(self.__width * self.__height)
+        return (self.__width * self.__height)
 
     def __str__(self):
-        return"[Rectangle] {}/{}".format(self.__width, self.__height)
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
