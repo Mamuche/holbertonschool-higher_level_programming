7fb04794b94c65dfcf345a2963fe62e3a552ab40
Mamuche <marionlaroche17@gmail.com>
2024-02-16 17:17:28 +0100

    requierements test

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,4 +5,7 @@
 def is_same_class(obj, a_class):
     """return true if the object is exactly
     an instance of the class,  or false"""
-    return (type(obj) == a_class)
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
