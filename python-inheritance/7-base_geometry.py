cf2e40deafec51752146e0a0b44327e3ffb93db0
Mamuche <marionlaroche17@gmail.com>
2024-02-18 10:53:17 +0100

    requierements

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-
-
 """add public instance method def integer_validator"""
 
 
 class BaseGeometry:
-
+    """class BaseGeometry"""
     def area(self):
+        """define the area"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """that validates value"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
