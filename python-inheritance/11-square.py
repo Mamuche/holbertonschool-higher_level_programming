6592f7f3e14fc28d68bf985a272eeb14c3af8ca8
Mamuche <marionlaroche17@gmail.com>
2024-02-18 18:06:30 +0100

    replace width/size

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -6,8 +6,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """new class inherits from Rectangle"""
     def __init__(self, size):
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
@@ -16,6 +16,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """return the square description"""
-        width = self.__size
-        height = self.__size
-        return "[Square] {}/{}".format(width, height)
+        return "[Square] {}/{}".format(self.__size, self.__size)
