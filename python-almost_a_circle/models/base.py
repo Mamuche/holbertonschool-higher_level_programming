912992e2f7c2b8ba5133c0111d1f0b1ba4362b28
Mamuche <marionlaroche17@gmail.com>
2024-02-20 11:04:10 +0100

    task1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""create class Base"""
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
