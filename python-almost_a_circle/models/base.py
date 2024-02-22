c72e3c35ec46b4115f6bf186832ff5d38f46f40e
Mamuche <marionlaroche17@gmail.com>
2024-02-22 20:41:54 +0100

    task17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,3 +33,11 @@ class Base:
                 lo.append(cls.to_dictionary(i))
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(lo))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or not json_string:
+            return ([])
+        else:
+            return json.loads(json_string)
