3a394fd1575794f3172ec0fa324b1c3edd869707
Mamuche <marionlaroche17@gmail.com>
2024-02-22 16:42:02 +0100

    task11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,10 +8,22 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """constructor"""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     def __str__(self):
         """return the rectangle description"""
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y,
                                                  self.width)
+    
+    """decorator for size"""
+    @property
+    def size(self):
+        return self.width
+    
+    """setter for size"""
+    @size.setter
+    def size(self, value):
+        self.validator(width=value)
+        self.validator(height=value)
+        self.width = value
+        self.height = value
